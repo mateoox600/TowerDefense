73066097ddee6b82d32dc24b8a40da8f4d33ba88
mateoox600 <matboi862@gmail.com>
2022-07-25 18:26:03 +0200

    Adding new enemy

---
+++ src/enemies/Medium.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../Enemy.hpp"
+
+class Medium : public Enemy {
+private:
+    
+public:
+    Medium(float startProgress) : Enemy(startProgress, 0.01f, 5.0f, GREEN) {
+        
+    }
+};
