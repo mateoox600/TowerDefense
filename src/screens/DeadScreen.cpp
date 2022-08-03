af78b14ceaa49025f46025adf28d538004d3f520
mateoox600 <matboi862@gmail.com>
2022-08-03 19:32:19 +0200

    Changing death screen gradient color Adding comment for update functions

--- src/screens/DeadScreen.cpp
+++ src/screens/DeadScreen.cpp
@@ -12,13 +12,24 @@ void DeadScreen::update() {
 }
 
 void DeadScreen::draw() {
-    DrawCircleGradient(screenWidth / 2, screenHeight / 2, screenWidth, WHITE, RED);
 
+    // Ui
+    //----------------------------------------------------------------------------------
+
+    DrawCircleGradient(screenWidth / 2, screenHeight / 2, screenWidth, raylib::Color(255, 230, 230), RED);
+
+    // "You died !" text
+    //----------------------------------------------------------------------------------
     std::string diedStr = "You died !";
     float diedWidth = MeasureText(diedStr.c_str(), footerSize);
     DrawText(diedStr.c_str(), screenWidth / 2 - diedWidth / 2, screenHeight / 2 - footerSize / 2, footerSize, BLACK);
 
+    // "Press ENTER to restart the game" text
+    //----------------------------------------------------------------------------------
     std::string restartStr = "Press ENTER to restart the game";
     float restartWidth = MeasureText(restartStr.c_str(), cellSize);
     DrawText(restartStr.c_str(), screenWidth / 2 - restartWidth / 2, screenHeight / 2 + footerSize / 2, cellSize, BLACK);
+
+    //----------------------------------------------------------------------------------
+    // Ui end
 }
--- src/screens/PlayingScreen.cpp
+++ src/screens/PlayingScreen.cpp
@@ -14,9 +14,18 @@ void PlayingScreen::update() {
     if(global.lives <= 0) {
         global.gameState = Dead;
     }
+
+    // Debug
+    //----------------------------------------------------------------------------------
+    /*
+    if(IsKeyPressed(KEY_SPACE)) global.lives = 0;
+    */
 }
 
 void PlayingScreen::draw() {
+    
+    // Grid
+    //----------------------------------------------------------------------------------
     for (size_t i = 0; i < mapSize.x + 1; i++) {
         DrawLine(cellSize * i, 0, cellSize * i, screenHeight, BLACK);
     }
@@ -24,14 +33,23 @@ void PlayingScreen::draw() {
         DrawLine(0, cellSize * j, screenWidth, cellSize * j, BLACK);
     }
 
+    // Ui
+    //----------------------------------------------------------------------------------
+
     DrawRectangle(0, screenHeight - footerSize, screenWidth, footerSize, GRAY);
 
+    // Lives left
+    //----------------------------------------------------------------------------------
     std::string livesLeft = TextFormat("%i", global.lives);
     float livesLeftTextWidth = raylib::MeasureText(livesLeft, footerSize - 20);
+
     DrawRectangle(screenWidth - footerSize - 30, screenHeight - footerSize + 10 , footerSize + 20, footerSize - 20, DARKGRAY);
+
     float livesLeftXPosition = screenWidth - footerSize - 30 + (footerSize + 20) / 2;
     raylib::DrawText(livesLeft, livesLeftXPosition - livesLeftTextWidth / 2, screenHeight - footerSize + 15, footerSize - 20, WHITE);
 
+    // Money
+    //----------------------------------------------------------------------------------
     std::string moneyString = TextFormat("%i", global.money);
     float moneyStringTextWidth = raylib::MeasureText(moneyString, footerSize - 20);
 
@@ -40,6 +58,12 @@ void PlayingScreen::draw() {
     DrawRectangle(moneyStringPosition.x - 10, moneyStringPosition.y, moneyStringTextWidth + 20, footerSize - 20, DARKGRAY);
     
     raylib::DrawText(moneyString, moneyStringPosition.x, moneyStringPosition.y + 5, footerSize - 20, WHITE);
+
+    //----------------------------------------------------------------------------------
+    // Ui end
+
+    // Managers
+    //----------------------------------------------------------------------------------
 
     global.pathManager.draw();
     global.enemyManager.draw();
